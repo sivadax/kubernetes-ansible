bc7232a7bb8d3942c4289250fbd68e3d9806b3bb
SiVA <sivadaskramdas@gmail.com>
2020-07-22 02:39:41 +0530

    Update initgit.sh

--- initgit.sh
+++ initgit.sh
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 echo "# kubernetes-ansible" >> README.md
 git init
 git add README.md
 git commit -m "first commit"
 git remote add origin https://github.com/sivadax/kubernetes-ansible.git
 git push -u origin master
-=======
-echo "# kubernetes-ansible" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/sivadax/kubernetes-ansible.git
-git push -u origin master
->>>>>>> d304a71d06a42e559ca06fd84a07d51443104dc7
