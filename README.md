d304a71d06a42e559ca06fd84a07d51443104dc7
SiVA <sivadaskramdas@gmail.com>
2020-07-22 00:00:18 +0530

    Add files via upload
    manual upload as git failed.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # kubernetes-ansible
+<<<<<<< HEAD
+=======
 
 install kubernetes on virtual box
+>>>>>>> b3161c75b476811267ab78f75dab5cbd0e3c5046
---
+++ initgit.sh
@@ -0,0 +1,6 @@
+echo "# kubernetes-ansible" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/sivadax/kubernetes-ansible.git
+git push -u origin master
